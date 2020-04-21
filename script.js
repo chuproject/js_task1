bcdb59ed1bfbe5b71ad8683ca9beea7d3992e019
AleksChu <alchuproject@gmail.com>
2020-04-21 20:27:29 +0300

    Task complected

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+let money = prompt('Ваш бюджет на месяц?', '');
+let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let expenses = {};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4;
+
+
+
+alert('Ваш бюджет на 1 день: ' + money / 30);
+
+
+console.log(appData);
+
